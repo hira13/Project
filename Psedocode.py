c3d75217d894f05d80184dc3eda748273e2440f6
hirangi <hira13@gmail.com>
2015-02-13 20:13:34 +0000

    Added the psuodo code to the file from sublime
    Tells the basic format of how I will execute my project

---
+++ Psedocode.py
@@ -0,0 +1,11 @@
+#Get Data of Endangered or higher level chordate species around the world 
+	#use the website scaper (what we learnt last wee) to get this information 
+	#Write a script to narrow down what type of chordate you want to look at 
+	#Script to get the type of threat/conservation that is present to this 1 type of species 
+	#Script to sort the type of threat/conservation method by category for all types of species 
+	#get total number of threats/conservation methods present based on type of threat 
+#Graph(what we will learn next week) 
+	#Bar graph the most common threats presented to the type of chordate selected 
+	#Can also bar graph the most common conservation methods for specific type of chordate species 
+
+
